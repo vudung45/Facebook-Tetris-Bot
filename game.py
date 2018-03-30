514d8b77152bf56c492362cf86435183e68cfde0
vudung45 <vudung45@yahoo.com>
2018-03-30 16:20:15 -0500

    Testing. Fixing bugs

--- game.py
+++ game.py
@@ -132,14 +132,8 @@ blocks = {
 # then generate a virtual board 
 class Game(object):
 	def __init__(self, width, height):
-		self.board = numpy.zeros(shape=(width + 2,height + 1))
-		#creating bounds
-		for i in range(0,width+2):
-			board[height][i] = 1;
-
-		for i in range(0,height + 1):
-			board[i][0] = 1;
-			board[i][width+1] = 1;
+		self.board = np.zeros(shape=(height + 1,width))
+	
 		self.saved_block = 0 #not yet implemented
 
 
@@ -149,20 +143,25 @@ class Game(object):
 		#iterate through all the possible form of the block
 		#find the best form and position to place it
 		for i, form in enumerate(blocks[block]):
-			opt_move = self.find_opt_move(i,form) #most optimal move for this form
+			opt_move = self.find_opt_move(form) #most optimal move for this form
 			#opt_move = (x_cord, fitness)
 			#comparing fitness value
 			if opt_move[1] > best_move[2]:
 				best_move = (i, opt_move[0], opt_move[1])
 		#place the puzzle
-		self.place_puzzle(self.board, best_fit[1], blocks[block][best_move[0]])
+		self.place_puzzle(self.board, best_move[1], blocks[block][best_move[0]])
 		return best_move
 
 
 	def collides(self, board, puzzle, x, y):
 		for r in range(0, len(puzzle)):
 			for c in range(0, len(puzzle[0])):
-				if(board[y + r][x +c] + puzzle[r][c] > 1):
+				val = 0
+				if y + r >= len(board) or x + c >= len(board[0]):
+					val = 1
+				else:
+					val = board[y+r][x+c]
+				if(val + puzzle[r][c] > 1):
 					return True
 		return False
 
@@ -171,37 +170,39 @@ class Game(object):
 	def place_puzzle(self, n_board, x, puzzle):
 		y = len(n_board) - 1
 		#looping from bottom to top for efficiency
-		while y > 0 and not collides(n_board,puzzle,x,y):
+		while y > 0 and self.collides(n_board,puzzle,x,y):
 			y-=1;
 
 		#place the puzzle
+		#print(y)
 		for r in range(0, len(puzzle)):
 			for c in range(0, len(puzzle[0])):
-				board_n[y+r][x+c] = puzzle[r][c];
+				if y + r < len(n_board) and x + c < len(n_board[0]):
+					n_board[y+r][x+c] = n_board[y+r][x+c] + puzzle[r][c];
 
 
 	#return points scored
 	#also clear rows
 	def rows_cleared(self,board, height = None):
-		points = 0;
 		rows_to_clear = []
-		r_bound = len(board)
+		r_bound = 0
 		if height != None:
-			r_bound = height + 1;
-		for r in reversed(range(0, r_bound)):
+			r_bound = len(board) - height;
+
+		for r in reversed(range(r_bound, len(board))):
 			for c in range(0, len(board[0])):
 				if(board[r][c] == 0):
 					break;
 				elif c == len(board[0]) - 1:
-					rows_to_clear.push(r);
+					rows_to_clear.append(r);
 		np.delete(board,rows_to_clear, axis = 0);
-		return points;
+		return len(rows_to_clear), np.delete(board,rows_to_clear, axis = 0)
 
 	def max_height(self, board):
 		for r in reversed(range(0, len(board))):
 			for c in range(0, len(board[0])):
 				if board[r][c] == 1:
-					return r;
+					return len(board) - r;
 
 		return 0;
 
@@ -224,13 +225,14 @@ class Game(object):
 		return sum
  	
  	#number of holes in the board
- 	def num_holes(self, board, height = None):
- 		holes = 0;
- 		if height != None
-			r_bound = height + 1;
-		for r in range(0, r_bound - 1):
+	def num_holes(self, board, height = None):
+		holes = 0
+		r_bound = 1;
+		if height != None:
+			r_bound = len(board) - height
+		for r in reversed(range(r_bound, len(board))):
 			for c in range(0, len(board[0])):
-				if board[r][c] == 0 and board[r+1][c] == 1:
+				if board[r][c] == 0 and board[r-1][c] == 1:
 					holes+=1;
 		return holes;
 				
@@ -243,19 +245,19 @@ class Game(object):
 			new_board = np.copy(self.board)
 			self.place_puzzle(new_board, x, puzzle)
 			m_height = self.max_height(new_board)
-			rows_cleared = rows_cleared(new_board, height = m_height)
+			rows_cleared , new_board = self.rows_cleared(new_board, height = m_height)
 			m_height -= rows_cleared
 			heights = self.all_heights(new_board)
 			cumulative_height = np.sum(heights)
 			relative_height = m_height - np.min(heights)
-			roughness = self.sumPars(heights)
-			holes = selfnum_holes(board,height = m_height)
+			roughness = self.sumPairs(heights)
+			holes = self.num_holes(new_board,height = m_height)
 			# fitness = Score = A * Sum of Heights
     		#             +  B * Number of Clears
     		#             +  C * Number of Holes 
     		#             +  D * Number of Blockades 
-			fitness = rows_cleared * 0.22 + m_height * -0.87 
-						+ cumulative_height * -0.73 + relative_height * 0.178 - holes * 0.15 + roughness * -0.02
+			fitness = rows_cleared * 0.22 + m_height * -0.87 \
+			+ cumulative_height * -0.73 + relative_height * 0.178 - holes * 0.15 + roughness * -0.02
 			if best_position[1] < fitness:
 				best_position = (x,fitness)
 		return best_position
---
+++ game_test.py
@@ -0,0 +1,7 @@
+from game import Game
+
+session = Game(16,50);
+
+
+session.get_move(1);
+print(session.board)
