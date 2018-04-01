3fd56f4fa88fc3e430e60f13707d3670583516cd
vudung45 <vudung45@yahoo.com>
2018-03-31 22:56:04 -0500

    More efficient, change screenshot method

--- tetris_ai.py
+++ tetris_ai.py
@@ -1,35 +1,48 @@
 import cv2
+import mss
+import mss.tools
 from PIL import ImageGrab
 import numpy as np
 from game import Game
 import time
 import pyautogui as pyg
-import keyboard
-ss = ImageGrab.grab();
-
-screen_frame=  cv2.resize(np.array(ss), (1680,1050))
+monitor = {'top': 380, 'left': 180, 'width': 180, 'height': 360}
+sct = mss.mss()
+ss = cv2.resize(np.array(sct.grab(monitor)),(monitor['width'],monitor['height']));
 
 #first_piece_region = cv2.selectROI(screen_frame);
-first_piece_region = (383,402, 20, 20)
+first_piece_region = (70, 0, 20, 20)
 next_piece_region = (534,350, 20, 20)
-gray = cv2.cvtColor(screen_frame, cv2.COLOR_RGB2GRAY)
-template = cv2.resize(cv2.imread('start.jpg',0),(100,30));
+gray = cv2.cvtColor(ss, cv2.COLOR_RGB2GRAY)
+template = cv2.resize(cv2.imread('start.jpg',0), (100, 30));
 
 res = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
 loc = np.where(res >= 0.5)
-print(loc[1][0],loc[0][0])
-pyg.click(loc[1][0],loc[0][0])
-pyg.click(loc[1][0],loc[0][0])
+pyg.click(monitor['left'] + loc[1][0], monitor['top'] + loc[0][0])
+pyg.click(monitor['left'] + loc[1][0], monitor['top'] + loc[0][0])
 
+# #RGB
+# color_to_piece = {
+# 	0 : [59, 168, 199,255], #I block light blue 0
+# 	1 : [212, 69, 95 ,255], #z block red 1
+# 	2 : [107, 181, 59 ,255], 	#s block green 2
+# 	3 : [214, 174, 68, 255], #square block yellow 3
+# 	4 : [214, 120, 58, 255], # L orange 4
+# 	5 : [64, 105, 197, 255], # reverse L dark blue 5
+# 	6 : [187, 72, 168, 255], # t block purple 6
+# 	7 : [0, 0, 0 , 255] # no-block black
+# }
+
+# BGR
 color_to_piece = {
-	0 : [59, 168, 199,255], #I block light blue 0
-	1 : [212, 69, 95 ,255], #z block red 1
-	2 : [107, 181, 59 ,255], 	#s block green 2
-	3 : [214, 174, 68, 255], #square block yellow 3
-	4 : [214, 120, 58, 255], # L orange 4
-	5 : [64, 105, 197, 255], # reverse L dark blue 5
-	6 : [187, 72, 168, 255], # t block purple 6
-	7 : [0, 0, 0 , 255] # no-block black
+	0 : [199, 168, 59, 255], #I block light blue 0
+	1 : [95, 69, 212, 255], #z block red 1
+	2 : [59, 181, 107, 255], 	#s block green 2
+	3 : [68, 174, 214, 255], #square block yellow 3
+	4 : [58, 120, 214, 255], # L orange 4
+	5 : [197, 105, 64, 255], # reverse L dark blue 5
+	6 : [168, 72, 187, 255], # t block purple 6
+	7 : [0, 0, 0, 255] # no-block black
 }
 
 def match_block(input_block):
@@ -45,39 +58,40 @@ def move_piece(move):
 	while move[0] > 0:
 		pyg.keyDown("up")
 		pyg.keyUp("up")
-		time.sleep(0.02)
+		time.sleep(0.01)
 		move[0]-= 1
 
 	if(move[1] > 4):
 		while move[1] > 4:
 	 		pyg.keyDown("right")
 	 		pyg.keyUp("right")
-	 		time.sleep(0.02)
+	 		time.sleep(0.01)
 	 		move[1]-=1
 	else:
 		while move[1] < 4:
 			pyg.keyDown("left")
 			pyg.keyUp("left")
-			time.sleep(0.02)
+			time.sleep(0.01)
 			move[1]+=1
 	pyg.keyDown("space")
-	time.sleep(0.01)
 	pyg.keyUp("space")
 
 game_started = False
 
 weights = [0.22568649650722883, -0.08679520494876472, \
-			-0.6152727732730796, 0.05842464424735841, \
-			-0.55452215909537684, -0.021586109522043928]
+			-0.6152727732730796, 0.15842464424735841, \
+			-0.75452215909537684, -0.021586109522043928]
 
-session = Game(10,10, weights)
-time.sleep(4.2)
+
+
+session = Game(10,30, weights)
+time.sleep(7)
+#time.sleep(4.5)
 next_piece = 7
 while(True):
-	ss = ImageGrab.grab();
-	screen_frame=  cv2.resize(np.array(ss), (1680,1050))
+	ti = time.time()
+	screen_frame = cv2.resize(np.array(sct.grab(monitor)),(monitor['width'],monitor['height']));
 	move = None
-	# if not game_started:
 	x,y,w,h = first_piece_region
 	input_block = screen_frame[y :y + h, x:x + w]
 	avg_color = np.mean(input_block, axis=(0,1))
@@ -86,19 +100,7 @@ while(True):
 		print("block_type:" + str(block_type))
 		#game_started = True
 		moves = session.get_move(block_type)
-		print(moves)
+		#print(moves)
 		move_piece(moves)
-	# else:
-	# 	move = session.get_move(next_piece)
-	# 	print(move)
-
-	# if game_started:
-	# 	x,y,w,h = next_piece_region
-	# 	nput_block = screen_frame[y :y + h, x:x + w]
-	# 	avg_color = np.mean(input_block, axis=(0,1))
-	# 	block_type = match_block(avg_color)
-	# 	if(block_type != 7):
-	# 		print("next_piece:" + str(block_type))
-	# 		next_piece = block_type
 
 
